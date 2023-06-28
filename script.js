aa64c7675eb8381a78fb73003747e546b5fc2f40
Marco Antônio "xan <129339274+marcoxandevelopment@users.noreply.github.com>
2023-06-28 09:26:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+ScrollReveal({
+  reset: true,
+  distance: "200px",
+  duration: 1000,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".container-main-content", {
+  delay: 800,
+});
+
+ScrollReveal().reveal(".container-work-text", {
+  delay: 300,
+  origin: "bottom",
+});
+
+ScrollReveal().reveal(".img1", {
+  delay: 100,
+  origin: "left",
+});
+
+ScrollReveal().reveal(".img2", {
+  delay: 200,
+  origin: "left",
+});
+
+ScrollReveal().reveal(".img3", {
+  delay: 300,
+  origin: "left",
+});
+ScrollReveal().reveal(".img4", {
+  delay: 400,
+  origin: "left",
+});
+ScrollReveal().reveal(".img5", {
+  delay: 500,
+  origin: "left",
+});
+ScrollReveal().reveal(".img6", {
+  delay: 600,
+  origin: "left",
+});
+ScrollReveal().reveal(".img7", {
+  delay: 700,
+  origin: "left",
+});
+ScrollReveal().reveal(".img8", {
+  delay: 800,
+  origin: "left",
+});
+
+ScrollReveal().reveal(".how-orks-img", {
+  delay: 400,
+  origin: "left",
+});
+
+ScrollReveal().reveal(".how-works-text", {
+  delay: 800,
+  origin: "left",
+});
+
+ScrollReveal().reveal(".ctn-act", {
+  delay: 800,
+  origin: "bottom",
+});
